9670ccc0331bdbe4f05fcbafe75038b8482d2d44
Spermotron <Spencerfermo17@gmail.com>
2024-10-06 18:19:24 -0700

    Anki collection backup on 2024-10-06 18:19:24

--- anki_backup.sh
+++ anki_backup.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-# Export Anki deck to the specified folder
-anki --export ~/Documents/AnkiExports/my_anki_deck.apkg
+# Set the directory where the Anki collection will be exported
+EXPORT_DIR="/home/YourUsername/Documents/AnkiExports"
 
-# Git commands to commit and push the exported deck
-cd ~/Documents/AnkiExports
-git add *.apkg
-git commit -m "Automated export and backup of Anki deck"
+# Set the Anki collection file path (modify this if your path is different)
+ANKI_PROFILE_DIR="/home/YourUsername/Anki2/YourProfile"
+
+# Export the Anki collection (all decks and media)
+echo "Exporting Anki collection to $EXPORT_DIR"
+cp "$ANKI_PROFILE_DIR/collection.anki2" "$EXPORT_DIR/anki_collection_backup.apkg"
+
+# Navigate to the export directory
+cd $EXPORT_DIR
+
+# Add all new or modified files in AnkiExports
+git add .
+
+# Commit the changes with a message including the date and time
+NOW=$(date +"%Y-%m-%d %T")
+git commit -m "Anki collection backup on $NOW"
+
+# Push the changes to GitHub
 git push origin main
+
+echo "Backup and GitHub push completed successfully."
+
